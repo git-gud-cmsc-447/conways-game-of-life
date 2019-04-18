0313f6472a6e1d8d17d81bedd06a709997291c34
Drew Barrett <dbarret1@umbc.edu>
2019-04-18 19:50:28 -0400

    increase base size

--- src/js/demo.js
+++ src/js/demo.js
@@ -14,7 +14,7 @@ const defaultOptions = {
   hideButtonSelector: '#ctrl-hide-show',
   switchEngineSelector: '#ctrl-engine',
   desiredFPS: 30,
-  pixelsPerCell: 5,
+  pixelsPerCell: 10,
   strokeStyle: 'rgba(255,118,5,0.5)',
   fillStyle: 'rgba(222,122,39,0.5)',
   showText: true,
